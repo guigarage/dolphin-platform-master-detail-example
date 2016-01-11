760156f1ba23cc7fd8b57eca8d2470eb908d807c
Hendrik Ebbers <hendrik.ebbers@web.de>
2016-01-11 13:33:24 +0100

    new application class

--- client/src/main/java/com/guigarage/dolphin/ClientApplication.java
+++ client/src/main/java/com/guigarage/dolphin/ClientApplication.java
@@ -1,36 +1,26 @@
 package com.guigarage.dolphin;
 
-import com.canoo.dolphin.client.ClientConfiguration;
 import com.canoo.dolphin.client.ClientContext;
-import com.canoo.dolphin.client.ClientContextFactory;
-import com.canoo.dolphin.client.javafx.JavaFXConfiguration;
+import com.canoo.dolphin.client.javafx.DolphinPlatformApplication;
 import com.guigarage.dolphin.view.DetailView;
 import com.guigarage.dolphin.view.MasterView;
-import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
-public class ClientApplication extends Application {
-
-    private ClientContext clientContext;
-
-    private MasterView masterView;
-
-    private DetailView detailView;
+public class ClientApplication extends DolphinPlatformApplication {
 
     @Override
-    public void init() throws Exception {
-        ClientConfiguration config = new JavaFXConfiguration("http://localhost:8080/dolphin");
-        clientContext = ClientContextFactory.connect(config).get();
+    protected String getServerEndpoint() {
+        return "http://localhost:8080/dolphin";
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        masterView = new MasterView(clientContext);
-        detailView = new DetailView(clientContext);
+    protected void start(Stage primaryStage, ClientContext clientContext) throws Exception {
+        MasterView masterView = new MasterView(clientContext);
+        DetailView detailView = new DetailView(clientContext);
         masterView.selectedStockIdentProperty().addListener((obs, oldVal, newVal) -> detailView.showStock(newVal));
 
         HBox mainPane = new HBox(masterView.getBaseNode(), detailView.getBaseNode());
@@ -41,14 +31,6 @@ public class ClientApplication extends Application {
 
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.show();
-    }
-
-    @Override
-    public void stop() throws Exception {
-        masterView.destroy().get();
-        detailView.destroy().get();
-        clientContext.disconnect().get();
-        System.exit(0);
     }
 
     public static void main(String... args) {
